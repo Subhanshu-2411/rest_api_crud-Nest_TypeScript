a8a86ca32f0e57d43f3c8a8ea6b6835f153e9d29
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-03-29 22:50:07 +0530

    Populted Controller File

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param , Delete, NotFoundException } from '@nestjs/common';
+import {UsersService} from "./users.service";
+import { User } from "./user.entity";
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Get()
+    async findAll(): Promise<User[]> {
+        return this.usersService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: number): Promise<User> {
+        const user = await this.usersService.findOne(id);
+        if(!user) {
+            throw new NotFoundException("User Does Not Exist");
+        }
+        else return user;
+    }
+
+    @Post()
+    async create(@Body() user: User): Promise<User> {
+        return this.usersService.create(user);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id:number, @Body() user: User): Promise<any> {
+        return this.usersService.update(id, user);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: number): Promise<any> {
+        const user = await this.usersService.findOne(id);
+        if(!user) {
+            throw new NotFoundException("User Does Not Exist");
+        }
+
+        else return this.usersService.delete(id);
+    }
+
+
+
+}
